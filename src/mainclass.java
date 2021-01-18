dd6d36403c92fea00549534362798cb922b75e9e
AndrewMayor <42546103+AndrewMayor@users.noreply.github.com>
2021-01-18 15:05:03 +0200

    Fixed loop to 20 iterations added counter

--- src/mainclass.java
+++ src/mainclass.java
@@ -1,7 +1,7 @@
 public class mainclass {
     public static void main(String [] args){
-        for (int i = 0; i < 10; i++) {
-            System.out.println("test");
+        for (int i = 0; i < 20; i++) {
+            System.out.println("test"+i);
         }
     }
 }
