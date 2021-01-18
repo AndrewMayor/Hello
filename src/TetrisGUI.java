d9e54d2484cedc018dfe3fec501465b04e442bfe
DESKTOP-7935ER9\dekul <dekulyk@gmail.com>
2021-01-18 19:14:50 +0200

    Deleted comments, added TetrisGUI.form

--- src/TetrisGUI.java
+++ src/TetrisGUI.java
@@ -1,14 +1,20 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 public class TetrisGUI extends JFrame {
-    private JButton button1;
-    private JTextField textField1;
     private JPanel TetrisPanel;
+    private JTable table1;
+
 
     public TetrisGUI(String title){
         super (title);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(TetrisPanel);
+        this.table1.setModel(new DefaultTableModel(
+                null,
+                new String[]{"test1","test2"}
+        ));
         this.pack();
     }
 
