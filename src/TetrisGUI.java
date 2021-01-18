150034096802f335b62bda74d9fd3f26d790e559
AndrewMayor <42546103+AndrewMayor@users.noreply.github.com>
2021-01-18 18:10:48 +0200

    Deleted old test classes. Added Swing form mockup

---
+++ src/TetrisGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class TetrisGUI extends JFrame {
+    private JButton button1;
+
+    public static void main(String [] args){
+        JFrame test = new JFrame();
+        test.setVisible(true);
+    }
+}
