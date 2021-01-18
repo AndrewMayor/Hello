9e54aa202409843142bf5d70e1aa8a0e0a4437bc
AndrewMayor <42546103+AndrewMayor@users.noreply.github.com>
2021-01-18 15:02:44 +0200

    Added test text output in a loop

--- src/mainclass.java
+++ src/mainclass.java
@@ -1,5 +1,7 @@
 public class mainclass {
     public static void main(String [] args){
-        System.out.println("test");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("test");
+        }
     }
 }
