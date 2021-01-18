eaf71504747a2ff81becd204426553a0dddd64fb
AndrewMayor <42546103+AndrewMayor@users.noreply.github.com>
2021-01-18 18:35:22 +0200

    Added JPanel correct creation and inheritance. Fixed panel size and position

--- src/TetrisGUI.java
+++ src/TetrisGUI.java
@@ -2,9 +2,18 @@ import javax.swing.*;
 
 public class TetrisGUI extends JFrame {
     private JButton button1;
+    private JTextField textField1;
+    private JPanel TetrisPanel;
+
+    public TetrisGUI(String title){
+        super (title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(TetrisPanel);
+        this.pack();
+    }
 
     public static void main(String [] args){
-        JFrame test = new JFrame();
+        JFrame test = new TetrisGUI("Tetris");
         test.setVisible(true);
     }
 }
